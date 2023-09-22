32237fab779c00307d76a19882c04244ddc00d33
Nhat Loi <nhatloi120402@gmail.com>
2023-09-22 18:23:54 +0700

    firework effect in homepage

---
+++ src/public/javascript/script1.js
@@ -0,0 +1,50 @@
+window.addEventListener('scroll', function () {
+    var fireworkSection = document.getElementById('firework-section');
+
+    // Create the firework container div
+    var fireworkEffect1 = document.createElement('div');
+    fireworkEffect1.classList.add('firework');
+    fireworkEffect1.id = 'firework1';
+
+    var fireworkEffect2 = document.createElement('div');
+    fireworkEffect2.classList.add('firework');
+    fireworkEffect2.id = 'firework2';
+
+    var fireworkEffect3 = document.createElement('div');
+    fireworkEffect3.classList.add('firework');
+    fireworkEffect3.id = 'firework3';
+    // Create 12 explosion divs
+    for (var i = 1; i <= 12; i++) {
+        var explosionDiv = document.createElement('div');
+        explosionDiv.classList.add('explosion');
+        fireworkEffect1.appendChild(explosionDiv);
+    }
+
+    for (var i = 1; i <= 12; i++) {
+        var explosionDiv = document.createElement('div');
+        explosionDiv.classList.add('explosion');
+        fireworkEffect2.appendChild(explosionDiv);
+    }
+
+    for (var i = 1; i <= 12; i++) {
+        var explosionDiv = document.createElement('div');
+        explosionDiv.classList.add('explosion');
+        fireworkEffect3.appendChild(explosionDiv);
+    }
+
+    // Append the firework container to the document body
+
+    var rect = fireworkSection.getBoundingClientRect();
+
+    if (rect.top <= window.innerHeight && !fireworkSection.classList.contains('firework-triggered')) {
+        fireworkSection.classList.add('firework-triggered');
+        fireworkSection.appendChild(fireworkEffect1);
+        fireworkSection.appendChild(fireworkEffect2);
+        fireworkSection.appendChild(fireworkEffect3);
+        setTimeout(function () {
+            fireworkEffect1.remove();
+            fireworkEffect2.remove();
+            fireworkEffect3.remove();
+        }, 5000);
+    }
+});
